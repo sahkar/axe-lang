09ab0d675e6a3c2cb3c3f49968368102893c3106
Sahith <sahith.karra@gmail.com>
2024-12-27 23:12:02 -0800

    deploy: pydantic and dockerfile changes

--- api/main.py
+++ api/main.py
@@ -1,26 +1,29 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException
 from juliacall import Main as jl
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class CodeRequest(BaseModel): 
+    code: str
+
 @app.post("/interp")
-async def interp_code(request: Request):
+async def interp_code(request: CodeRequest):
     try:
-        body = await request.json()
-        code = body.get("code", "")
         jl.seval('include("./src/interp.jl")')
-        ast = jl.seval(f'Interp.topInterp(:({code}))')
+        ast = jl.seval(f'Interp.topInterp(:({request.code}))')
         return {"expr" : str(ast)}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/parse")
-async def parse_code(request: Request):
+async def parse_code(request: CodeRequest):
     try:
-        body = await request.json()
-        code = body.get("code", "")
         jl.seval('include("./src/parser.jl")')
-        ast = jl.seval(f'Parser.parse(:({code}))')
+        ast = jl.seval(f'Parser.parse(:({request.code}))')
         return {"expr" : str(ast)}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+
