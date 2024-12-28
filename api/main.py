28ad410222fe1e77705d947f35ff8cfa1b417733
Sahith <sahith.karra@gmail.com>
2024-12-27 22:06:04 -0800

    remove pydantic

--- api/main.py
+++ api/main.py
@@ -1,29 +1,26 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from juliacall import Main as jl
-from pydantic import BaseModel
 
 app = FastAPI()
 
-class CodeRequest(BaseModel): 
-    code: str
-
 @app.post("/interp")
-async def interp_code(request: CodeRequest):
+async def interp_code(request: Request):
     try:
+        body = await request.json()
+        code = body.get("code", "")
         jl.seval('include("./src/interp.jl")')
-        ast = jl.seval(f'Interp.topInterp(:({request.code}))')
+        ast = jl.seval(f'Interp.topInterp(:({code}))')
         return {"expr" : str(ast)}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/parse")
-async def parse_code(request: CodeRequest):
+async def parse_code(request: Request):
     try:
+        body = await request.json()
+        code = body.get("code", "")
         jl.seval('include("./src/parser.jl")')
-        ast = jl.seval(f'Parser.parse(:({request.code}))')
+        ast = jl.seval(f'Parser.parse(:({code}))')
         return {"expr" : str(ast)}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-
-
